39aa11f23aa71f2f792d029b10ec87c86799c849
VanshaTatvaSoft <tatva.pci217@outlook.com>
2025-07-22 15:11:50 +0530

    change in entry gaurd

--- src/app/guards/entry-guard.ts
+++ src/app/guards/entry-guard.ts
@@ -1,22 +1,28 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { catchError, map, of } from 'rxjs';
+import { catchError, map, of, switchMap } from 'rxjs';
 import { AuthApiService } from '../services/auth-service/auth-api.service';
 
 export const entryGuard: CanActivateFn = () => {
-  const loginService = inject(AuthApiService);
+  const authService = inject(AuthApiService);
   const router = inject(Router);
 
-  // if (!loginService.getRememberMe()) return;
-
-  return loginService.validateAndRefreshToken().pipe(
-    map(res => {
-      debugger
-      if (res.statusCode === 200) {
+  return authService.validateAccessToken().pipe(
+    switchMap((res) => {
+      if (res.isValid && !res.isExpired) {
         router.navigateByUrl('/dashboard');
-        return false;
+        return of(false);
       }
-      return true;
+      if (res.isExpired && res.isRememberMe) {
+        return authService.refreshToken().pipe(
+          map(() => {
+            router.navigateByUrl('/dashboard');
+            return false;
+          }),
+          catchError(() => of(true))
+        );
+      }
+      return of(true);
     }),
     catchError(() => of(true))
   );
